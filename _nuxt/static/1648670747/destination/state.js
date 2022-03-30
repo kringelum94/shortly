a6acab9b81f7d3108f27d57a1eb6a854564fb774
kringelum94 <kringelum94@users.noreply.github.com>
2022-03-30 20:12:35 +0000

    deploy: 5259f25fe2c225d3b0f47d5bc14a8226b252855d

--- _nuxt/static/1648670747/destination/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fspace-tourism\u002F_nuxt\u002Fstatic\u002F1648670747",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdestination",config:{_app:{basePath:"\u002Fspace-tourism\u002F",assetsPath:"\u002Fspace-tourism\u002F_nuxt\u002F",cdnURL:a}}}}(null));
