3140134646ccfa3c580e582a1442e944c3df996f
kringelum94 <kringelum94@users.noreply.github.com>
2022-04-22 21:59:14 +0000

    deploy: 72b43a02c40b782ee6b5e65ff7089ee6764b26a6

--- _nuxt/06ba4d0.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{248:function(t,e,n){"use strict";n.r(e);var l=n(42),component=Object(l.a)({},(function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",[n("h5",{staticClass:"text-[28px] font-serif text-white uppercase"},[t._t("default")],2)])}),[],!1,null,null,null);e.default=component.exports}}]);
--- _nuxt/static/1648672270/technology/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fspace-tourism\u002F_nuxt\u002Fstatic\u002F1648672270",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftechnology",config:{_app:{basePath:"\u002Fspace-tourism\u002F",assetsPath:"\u002Fspace-tourism\u002F_nuxt\u002F",cdnURL:a}}}}(null));
